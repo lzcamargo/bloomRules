48a52a47d9331dc48da6f4125c48802ab22a95a6
Luiz <camargho@gmail.com>
2018-03-13 10:51:39 -0300

    deleted:    CBStyle.rb 	deleted:    HPStyle.rb 	deleted:    bxrules.rb 	deleted:    conformto.rb 	deleted:    protocolmm.rb 	modified:   protocolmt.rb 	deleted:    qualify/CBStyle.rb 	deleted:    qualify/HPStyle.rb 	deleted:    qualify/bxrules.rb 	deleted:    qualify/conformto.rb 	deleted:    qualify/protocolmt.rb 	bidirectional.rb 	unidirectional.rb

--- protocolmt.rb
+++ protocolmt.rb
@@ -1,22 +1,23 @@
 module ProtocolBloomRules
 	state do # collections used on rules
-		table :source_parent_model, [:source_element, :source_parent, :source_value ]   
-		table :source_child_model, [:source_element, :source_parent, :source_value ]  
-		table :target_parent_model, [:target_element, :target_parent, :target_value ]   
-		table :target_child_model, [:target_element, :target_parent, :target_value ]  
+		table :src_model_element, [:source_element, :source_ref, :source_value ]   
+		table :src_model_subelmt, [:source_element, :source_ref, :source_value ]  
+		table :tgt_model_element, [:target_element, :target_ref, :target_value ]   
+		table :tgt_model_subelmt, [:target_element, :target_ref, :target_value ]  
 		
 		scratch :aux_persistent, [:aux_element, :aux_parent, :aux_value]
 		scratch :aux_parent, [:aux_element, :aux_parent, :aux_value]
 		scratch :aux_attribute, [:aux_element, :aux_parent, :aux_value]
-		table :backwd_parent_model, [:backwd_element, :backwd_parent, :backwd_value ]  
-		table :backwd_child_model, [:backwd_element, :backwd_parent, :backwd_value ]  
+		table :backwd_model_element, [:backwd_element, :backwd_ref, :backwd_value ]  
+		table :backwd_model_subelmt, [:backwd_element, :backwd_ref, :backwd_value ]  
 		
 		table :source_metamodel, [:mm_element, :mm_parent, :mm_valor]
 		table :target_metamodel, [:mm_element, :mm_parent, :mm_valor]
 	end
-  state do	# for receiving elements from mapping and conformity metamodels 
-		table :bloom_rule_name, [:lhs_name, :hs_name, :id_rule]
-		table	:conform_package, [:map_pck_element, :map_pck_value] 
-		table	:conform_class, [:map_cla_element, :map_cla_value]
-		table	:conform_attribute, [:map_atr_element, :map_atr_value]
+  state do	# collections used on conformity with metamodels and links 
+		table :bloom_rule_name, [:lhs_name, :rhs_name, :id_rule]
+		table	:link_pck_sch, [:lnk_src_element, :lnk_tgt_element] 
+		table	:link_cla_tab, [:lnk_src_element, :lnk_tgt_element]
+		table	:link_att_col, [:lnk_src_element, :lnk_tgt_element]
+	end	
 end
